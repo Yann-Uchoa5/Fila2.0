b1b0f1242e0bfd8d49fde31ca9124b1786d68023
Yan Uchôa <128929472+Yann-Uchoa5@users.noreply.github.com>
2024-02-18 14:56:03 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+// App.js
+import React from 'react';
+ // Certifique-se de ajustar o caminho do arquivo conforme necessário
+import Fila from './Fila';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      <Fila />
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
